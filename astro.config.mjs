1449f2dbecb7433ac84d335ffd120775036f2ca0
坂東希 <bandonozomi@bandounozominoMacBook-Pro-2.local>
2026-06-13 15:52:45 +0900

    setup astro github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,9 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({});
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://inunonanahenko.github.io",
+  base: "/omJobmatch-lp",
+});
